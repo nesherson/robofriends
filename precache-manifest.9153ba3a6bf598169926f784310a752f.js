a597e1b45c026e41ded019b9fc51e26ba451aa3c
unknown <nesad.omeragich27@gmail.com>
2020-04-02 18:05:54 +0200

    Updates

---
+++ precache-manifest.9153ba3a6bf598169926f784310a752f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecea381fbeee3774bbb2dce0ff56f45",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "adceaffef523daf2c659",
+    "url": "/robofriends/static/css/main.0af5ba06.chunk.css"
+  },
+  {
+    "revision": "541edbd8124ffd22746a",
+    "url": "/robofriends/static/js/2.bf9239df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.bf9239df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adceaffef523daf2c659",
+    "url": "/robofriends/static/js/main.98336d93.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
