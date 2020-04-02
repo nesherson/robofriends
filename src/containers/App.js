799d9d5a3f44d7d33e9a0f10ca8939e3bc8ee2c7
unknown <nesad.omeragich27@gmail.com>
2020-04-02 17:51:13 +0200

    Modified folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './app.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     };
   }
@@ -17,12 +16,21 @@ class App extends React.Component {
     this.setState({ searchField: event.target.value });
   };
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({ robots: data });
+      });
+  }
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
+
     return (
       <div className='main'>
         <h1 className='header'>Robofriends</h1>
