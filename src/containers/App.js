f1fee0b48bf42a7489115d4ad7f2252bd6fd0277
unknown <nesad.omeragich27@gmail.com>
2020-06-16 13:48:36 +0200

    added btn for adding random robot

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
+import { Robots } from '../robots.js';
 import './app.css';
+
+function returnRobot() {
+  return Robots[Math.floor(Math.random() * 10 - 2)];
+}
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
   const [robots, setRobots] = useState([]);
 
   useEffect(() => {
-    if (robots.length === 0) {
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then((resp) => resp.json())
-        .then((data) => {
-          const newRobots = data.map((user) => {
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-            };
-          });
-          setRobots(newRobots);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((resp) => resp.json())
+      .then((data) => {
+        const tempRobots = data.map((user) => {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          };
         });
-    }
-  });
+        setRobots(tempRobots);
+      });
+  }, []);
 
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
@@ -32,6 +35,9 @@ const App = () => {
     <div className='main'>
       <h1 className='header'>Robofriends</h1>
       <SearchBox searchChange={(e) => setSearchField(e.target.value)} />
+      <button onClick={() => setRobots([...robots, returnRobot()])}>
+        Click me
+      </button>
       <CardList robots={filteredRobots} />
     </div>
   );
