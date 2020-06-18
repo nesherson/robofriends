f5c0cc024f5f4c174eadc870249c2f1d66c52ec9
unknown <nesad.omeragich27@gmail.com>
2020-06-18 19:22:47 +0200

    code refactoring for custom robot

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+import CustomRobotCard from '../components/CustomRobotCard';
+import './cardList.css';
 
-const CardList = ({ robots }) => {
+const CardList = ({ searchField }) => {
+  const [robots, setRobots] = useState([]);
+  const [newRobot, setNewRobot] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const data = await response.json();
+      const tempRobots = data.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          email: item.email,
+        };
+      });
+      setRobots(tempRobots);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://api.randomuser.me/');
+      const data = await response.json();
+      const [item] = data.results;
+
+      setNewRobot({
+        id: item.registered.date,
+        name: item.name.first,
+        email: item.email,
+      });
+    };
+    fetchData();
+  }, [robots]);
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
   return (
-    <div>
-      {robots.map((robot) => {
-        return (
-          <Card
-            key={robot.id}
-            id={robot.id}
-            name={robot.name}
-            email={robot.email}
-          />
-        );
-      })}
+    <div className='cardList-wrapper'>
+      <button className='btn' onClick={() => setRobots([...robots, newRobot])}>
+        RANDOM
+      </button>
+      <div className='cardList'>
+        {filteredRobots.map((robot) => {
+          return (
+            <Card
+              key={robot.id}
+              id={robot.id}
+              name={robot.name}
+              email={robot.email}
+            />
+          );
+        })}
+        <CustomRobotCard
+          onSubmit={(name, email) => {
+            setRobots([
+              ...robots,
+              { id: new Date(), name: name, email: email },
+            ]);
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,68 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import CustomRobotCard from '../components/CustomRobotCard';
+
 import './app.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
-  const [robots, setRobots] = useState([]);
-  const [newRobot, setNewRobot] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      const data = await response.json();
-      const tempRobots = data.map((item) => {
-        return {
-          id: item.id,
-          name: item.name,
-          email: item.email,
-        };
-      });
-      setRobots(tempRobots);
-    };
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://api.randomuser.me/');
-      const data = await response.json();
-      const [item] = data.results;
-
-      setNewRobot({
-        id: item.registered.date,
-        name: item.name.first,
-        email: item.email,
-      });
-    };
-    fetchData();
-  }, [robots]);
-
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchField.toLowerCase());
-  });
+  /*<CustomRobotCard
+        onSubmit={(name, email) => {
+          setRobots([...robots, { id: new Date(), name: name, email: email }]);
+        }}
+      />*/
 
   return (
     <div className='main'>
       <h1 className='header'>Robofriends</h1>
       <SearchBox searchChange={(e) => setSearchField(e.target.value)} />
-      <button className='btn' onClick={() => setRobots([...robots, newRobot])}>
-        RANDOM
-      </button>
-      <CardList robots={filteredRobots}>
-        <CustomRobotCard
-          onSubmit={(name, email) => {
-            setRobots([
-              ...robots,
-              { id: new Date(), name: name, email: email },
-            ]);
-          }}
-        />
-      </CardList>
+      <CardList searchField={searchField} />
     </div>
   );
 };
